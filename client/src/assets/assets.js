49e3241f4ebbd72973fc26531f8fc95de242abfd
vishnu <vishnumathappan16@gmail.com>
2025-10-05 21:49:02 +0530

    hotel update

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -75,10 +75,10 @@ export const assets = {
 }
 
 export const cities = [
-    "Dubai",
-    "Singapore",
-    "New York",
-    "London",
+    "Erode",
+    "Salem",
+    "Namakkal",
+    "Coimbatore",
 ];
 
 // Exclusive Offers Dummy Data
